2748420ec33b8fca1e25a5f8044685cd826a1e13
bogdanilchenko1987 <71671855+bogdanilchenko1987@users.noreply.github.com>
2024-12-26 22:37:50 +0000

    Deploying to gh-pages from @ bogdanilchenko1987/react-phonebook@94789820805255ca9497eb338d1ae4033b2d9955 🚀

--- static/js/520.7de166f7.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[520],{4520:function(e,n,o){o.r(n),o.d(n,{default:function(){return c}});var t=o(1087),r=o(7133),u=o(184);function c(){return(0,u.jsxs)(r.UN,{children:["404 Not found. Return to ",(0,u.jsx)(t.rU,{to:"/",children:"HomePage"})]})}}}]);
-//# sourceMappingURL=520.7de166f7.chunk.js.map
